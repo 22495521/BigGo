b0515b19889ef3e912d6efd2b0c64bbfaee04b51
bigto <d22495521@gmail.com>
2023-02-17 14:28:44 +0800

    add AlilityComponent

--- src/views/front/FrontPage.vue
+++ src/views/front/FrontPage.vue
@@ -2,16 +2,19 @@
   <Banner-View></Banner-View>
   <ThinkingView></ThinkingView>
   <GoodratingView></GoodratingView>
+  <Ability-Component></Ability-Component>
 </template>
 <script>
 import BannerView from "../../components/front/BannerView.vue";
 import ThinkingView from "../../components/front/ThinkingView.vue";
 import GoodratingView from "../../components/front/ GoodratingView.vue";
+import AbilityComponent from "../../components/front/AbilityComponent.vue";
 export default {
   components: {
     BannerView,
     ThinkingView,
     GoodratingView,
+    AbilityComponent,
   },
 };
 </script>
