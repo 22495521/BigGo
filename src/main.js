3ace4c994b10b7497911ae81d0d4409c05fda69e
bigto <d22495521@gmail.com>
2023-02-17 11:14:26 +0800

    impot bs5

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import "bootstrap";
 // import './assets/main.css'
 import "./assets/helpers/all.scss";
 const app = createApp(App);
--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -116,7 +116,6 @@ export default {
     RouterView,
     RouterLink,
   },
-
 };
 </script>
 
