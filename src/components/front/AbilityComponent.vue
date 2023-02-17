e7f92b8d3bc3ec91bfbf0bebdb4cfd6dfae86a14
bigto <d22495521@gmail.com>
2023-02-17 22:10:22 +0800

    updata ability

--- src/components/front/AbilityComponent.vue
+++ src/components/front/AbilityComponent.vue
@@ -1,7 +1,65 @@
 <template>
   <p class="mb-0 fw-bold fs-2 text-center pt-5">圍棋提升的多維能力</p>
+  <div class="container px-2 px-md-0 d-flex flex-column overflow-hidden">
+    <div class="d-md-flex">
+      <div class="card-w">
+        <img
+          src="../../assets/image/首頁/Section_04_img01.jpg"
+          class="img-fluid"
+          alt=""
+        />
+      </div>
+      <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
+        <div class="px-md-3">
+          <h4>集中專注力</h4>
+          <p class="fs-4">
+            下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="d-md-flex flex-md-row-reverse">
+      <div class="card-w">
+        <img
+          src="../../assets/image/首頁/Section_04_img02.jpg"
+          class="img-fluid"
+          alt=""
+        />
+      </div>
+      <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
+        <h4>集中專注力</h4>
+        <p class="fs-4">
+          下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。
+        </p>
+      </div>
+    </div>
+    <div class="d-md-flex">
+      <div class="card-w">
+        <img
+          src="../../assets/image/首頁/Section_04_img03.jpg"
+          class="img-fluid"
+          alt=""
+        />
+      </div>
+      <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
+        <div class="px-md-3">
+          <h4>集中專注力</h4>
+          <p class="fs-4">
+            下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-
+.card-w {
+  width: 100%;
+}
+@media screen and (min-width: 768px) {
+  .card-w {
+    width: 100%;
+  }
+}
 </style>
