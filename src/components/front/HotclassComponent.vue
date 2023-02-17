5eeca6d71683508069c8aa3c5b9ab5a5413ff557
bigto <d22495521@gmail.com>
2023-02-17 15:58:34 +0800

    Hotclass add

---
+++ src/components/front/HotclassComponent.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="bg-info">
+    <p class="mb-0 fw-bold fs-2 text-center pt-5">熱門課程</p>
+    <div class="d-flex flex-column flex-md-row container">
+      <div class="col-md-4 px-3 py-4">
+        <div
+          class="card p-3"
+          style="width: 100%; border-radius: 20px; border: 3px solid #6f6f6f"
+        >
+          <img
+            src="../../assets/image/首頁/Section_05_img01.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body pb-0">
+            <div class="d-flex pb-0">
+              <h4 class="card-title lh-1" style="font-weight: 700">入門</h4>
+              <div class="ms-auto fs-6 text-decoration-line-through pb-0">
+                <p class="pb-0 pt-2 text-danger mb-0" style="font-size: 12px">
+                  NT$ 3000
+                </p>
+              </div>
+            </div>
+
+            <div class="card-text d-flex">
+              <p>適合：零基礎</p>
+              <p class="ms-auto">NT$ 2500</p>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button
+                href="#"
+                class="btn btn-primary btn btn-success rounded-pill"
+                style="
+                  padding: 3% 39%;
+                  border-radius: 20px;
+                  border: 3px solid #6f6f6f;
+                "
+              >
+                課程詳情
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 px-3 py-4">
+        <div
+          class="card p-3"
+          style="width: 100%; border-radius: 20px; border: 3px solid #6f6f6f"
+        >
+          <img
+            src="../../assets/image/首頁/Section_05_img01.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body pb-0">
+            <div class="d-flex pb-0">
+              <h4 class="card-title lh-1" style="font-weight: 700">入門</h4>
+              <div class="ms-auto fs-6 text-decoration-line-through pb-0">
+                <p class="pb-0 pt-2 text-danger mb-0" style="font-size: 12px">
+                  NT$ 3000
+                </p>
+              </div>
+            </div>
+
+            <div class="card-text d-flex">
+              <p>適合：零基礎</p>
+              <p class="ms-auto">NT$ 2500</p>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button
+                href="#"
+                class="btn btn-primary btn btn-success rounded-pill"
+                style="
+                  padding: 3% 39%;
+                  border-radius: 20px;
+                  border: 3px solid #6f6f6f;
+                "
+              >
+                課程詳情
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 px-3 py-4">
+        <div
+          class="card p-3"
+          style="width: 100%; border-radius: 20px; border: 3px solid #6f6f6f"
+        >
+          <img
+            src="../../assets/image/首頁/Section_05_img01.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body pb-0">
+            <div class="d-flex pb-0">
+              <h4 class="card-title lh-1" style="font-weight: 700">入門</h4>
+              <div class="ms-auto fs-6 text-decoration-line-through pb-0">
+                <p class="pb-0 pt-2 text-danger mb-0" style="font-size: 12px">
+                  NT$ 3000
+                </p>
+              </div>
+            </div>
+
+            <div class="card-text d-flex">
+              <p>適合：零基礎</p>
+              <p class="ms-auto">NT$ 2500</p>
+            </div>
+            <div class="d-flex justify-content-center">
+              <button
+                href="#"
+                class="btn btn-primary btn btn-success rounded-pill"
+                style="
+                  padding: 3% 39%;
+                  border-radius: 20px;
+                  border: 3px solid #6f6f6f;
+                "
+              >
+                課程詳情
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pt-3 py-5 justify-content-center d-flex">
+      <button
+        class="btn btn-primary rounded-pill"
+        style="padding: 15px 80px; font-size: 24px"
+      >
+        更多課程
+      </button>
+    </div>
+  </div>
+</template>
