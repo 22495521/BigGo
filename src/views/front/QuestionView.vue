534ca696b3a4ab267b61af3e5e6a2fcbb3bdd710
bigto <d22495521@gmail.com>
2023-02-18 14:32:01 +0800

    123

--- src/views/front/QuestionView.vue
+++ src/views/front/QuestionView.vue
@@ -3,7 +3,7 @@
     <div class="pt-5 pb-4">
       <h2 style="font-weight: 700">問與答</h2>
     </div>
-    <div class="accordion pb-5" id="accordionExample">
+    <div class="accordion pb-5 accordion-w" id="accordionExample">
       <div class="mb-4 shadow-sm rounded-3">
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingOne">
@@ -155,9 +155,9 @@
 .accordion-w {
   width: 60%;
 }
-@media screen and (min-width: 768px) {
+@media screen and (max-width: 768px) {
   .accordion-w {
-    width: 90%;
+    width: 100%;
   }
 }
 </style>
