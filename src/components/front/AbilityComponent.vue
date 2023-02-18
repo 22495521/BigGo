6b0b3a6dcb3d0746c06fb8ceb3c77d234f6d2e4b
bigto <d22495521@gmail.com>
2023-02-18 13:12:45 +0800

    bability border and px

--- src/components/front/AbilityComponent.vue
+++ src/components/front/AbilityComponent.vue
@@ -1,20 +1,18 @@
 <template>
   <p class="mb-0 fw-bold fs-2 text-center py-4">圍棋提升的多維能力</p>
-  <div class="container px-2 px-md-0 d-flex flex-column overflow-hidden">
+  <div class="container px-5 px-md-0 d-flex flex-column overflow-hidden">
     <div class="d-md-flex">
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img01.jpg"
-          class="img-fluid"
+          class="img-fluid card-radius"
           alt=""
         />
       </div>
       <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
-        <div class="px-md-3">
+        <div class="px-md-4">
           <h4>集中專注力</h4>
-          <p class="fs-4">
-            下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。
-          </p>
+          <p class="fs-4">下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。</p>
         </div>
       </div>
     </div>
@@ -22,31 +20,29 @@
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img02.jpg"
-          class="img-fluid"
+          class="img-fluid card-radius"
           alt=""
         />
       </div>
       <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
-        <h4>獨立思考</h4>
-        <p class="fs-4">
-          每一盤棋都有可能面臨困難的境地，需要學會不依賴任何人做出決定。
-        </p>
+        <div class="px-md-4">
+          <h4>獨立思考</h4>
+          <p class="fs-4">下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。</p>
+        </div>
       </div>
     </div>
     <div class="d-md-flex">
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img03.jpg"
-          class="img-fluid"
+          class="img-fluid card-radius"
           alt=""
         />
       </div>
       <div class="pt-3 d-md-flex flex-md-column justify-content-center card-w">
-        <div class="px-md-3">
+        <div class="px-md-4">
           <h4>集中專注力</h4>
-          <p class="fs-4">
-            下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。
-          </p>
+          <p class="fs-4">下一盤圍棋大約30分鐘至1小時，須全程集中精神、不可鬆懈。</p>
         </div>
       </div>
     </div>
@@ -57,9 +53,12 @@
 .card-w {
   width: 100%;
 }
-@media screen and (min-width: 768px) {
+@media screen and (max-width: 768px) {
   .card-w {
     width: 100%;
+  }
+  .card-radius {
+    border-radius: 32px;
   }
 }
 </style>
