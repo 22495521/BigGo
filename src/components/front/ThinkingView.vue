4b5cd853d3d35a0dcca3170d10b54c118150f341
bigto <d22495521@gmail.com>
2023-03-03 23:36:43 +0800

    1次

--- src/components/front/ThinkingView.vue
+++ src/components/front/ThinkingView.vue
@@ -78,7 +78,7 @@ export default {
       delay: 0, // values from 0 to 3000, with step 50ms
       duration: 1000,
       easing: "ease", // default easing for AOS animations
-      once: false, // whether animation should happen only once - while scrolling down
+      once: true, // whether animation should happen only once - while scrolling down
     });
   },
 };
