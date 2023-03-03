207a99d6b4198a8bc2474ec61c22e9c398b259c1
bigto <d22495521@gmail.com>
2023-03-03 23:35:50 +0800

    動畫

--- src/components/front/AbilityComponent.vue
+++ src/components/front/AbilityComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <p class="mb-0 fw-bold fs-2 text-center py-4">圍棋提升的多維能力</p>
   <div class="container px-5 px-md-0 d-flex flex-column overflow-hidden">
-    <div class="d-md-flex" data-aos="fade-left">
+    <div class="d-md-flex" data-aos="fade-up">
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img01.jpg"
@@ -18,7 +18,7 @@
         </div>
       </div>
     </div>
-    <div class="d-md-flex flex-md-row-reverse" data-aos="fade-right">
+    <div class="d-md-flex flex-md-row-reverse" data-aos="fade-up">
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img02.jpg"
@@ -35,7 +35,7 @@
         </div>
       </div>
     </div>
-    <div class="d-md-flex" data-aos="fade-left">
+    <div class="d-md-flex" data-aos="fade-up">
       <div class="card-w">
         <img
           src="../../assets/image/首頁/Section_04_img03.jpg"
