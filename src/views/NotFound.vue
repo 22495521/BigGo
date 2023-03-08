02b08830fa4b1fd49a83dbf15326df02742a70c1
bigto <d22495521@gmail.com>
2023-03-08 11:22:27 +0800

    修正

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,4 +1,17 @@
-<template>找不到404~~~~~</template>
+<template>
+  <div class="container text-center">
+    <img
+      src="../../src/assets/image/404頁面/Section_01_404_web.svg"
+      class="mt-6 img-fluid d-none d-md-inline-block text-center"
+      alt="404圖片web"
+    />
+    <img
+      src="../../src/assets/image/404頁面/Section_01_404_mobile.svg"
+      class="mt-6 img-fluid d-md-none"
+      alt="404圖片手機"
+    />
+  </div>
+</template>
 <script>
 export default {};
 </script>
