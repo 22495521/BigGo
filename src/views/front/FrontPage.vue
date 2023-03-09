4c9beadadeac83f339a1c7ff7958c591bca7d060
bigto <d22495521@gmail.com>
2023-03-09 22:25:12 +0800

    修改

--- src/views/front/FrontPage.vue
+++ src/views/front/FrontPage.vue
@@ -8,7 +8,7 @@
 <script>
 import BannerView from "../../components/front/BannerView.vue";
 import ThinkingView from "../../components/front/ThinkingView.vue";
-import GoodratingView from "../../components/front/ GoodratingView.vue";
+import GoodratingView from "../../components/front/GoodratingView.vue";
 import AbilityComponent from "../../components/front/AbilityComponent.vue";
 import HotclassComponent from "../../components/front/HotclassComponent.vue";
 export default {
